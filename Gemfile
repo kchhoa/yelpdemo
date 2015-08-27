8e1360c702436a888bc1a9d64907fb364c6864b3
kazuki kizawa <kazuki.chhoa@gmail.com>
2015-08-27 19:09:09 +0900

    Add search functionality with Searchkick

--- Gemfile
+++ Gemfile
@@ -33,6 +33,7 @@ gem "fog"
 gem 'figaro'
 gem "mini_magick"
 gem 'devise'
+gem 'searchkick'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,8 @@
 class Restaurant < ActiveRecord::Base
 	mount_uploader :image, ImageUploader
 
+  searchkick
+
 	has_many :reviews, dependent: :destroy
 
 	validates :name, :address, :phone, :website, :image, presence: true
